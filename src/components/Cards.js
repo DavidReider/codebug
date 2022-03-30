1f82a05872551e8c4220cb461c7f39a37a628b9c
DavidReider <freakdj1997@yahoo.com>
2022-03-30 19:32:27 -0400

    Pull in articles from sanity on homepage

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,31 +1,79 @@
-import React from 'react';
-import CardItem from './CardItem';
-import './Cards.css';
-import img9 from '../images/img-9.jpg';
-import img2 from '../images/img-2.jpg';
-import img3 from '../images/img-3.jpg';
-import img4 from '../images/img-4.jpg';
-import img8 from '../images/img-8.jpg';
+import React, { useState, useEffect } from "react";
+import CardItem from "./CardItem";
+import "./Cards.css";
+import sanityClient from "../client.js";
 
 function Cards() {
-    return (
-        <div className="cards">
-            <h1>Check Out These Featured Articles</h1>
-            <div className="cards__container">
-                <div className="cards__wrapper">
-                    <ul className="cards__items">
-                        <CardItem src={img9} text="Explore the wonderful uses of Technology" label="Technology" path="/blog"/>
-                        <CardItem src={img2} text="Just how much does social media affect your productivity?" label="Productivity" path="/blog"/>
-                    </ul>
-                    <ul className="cards__items">
-                        <CardItem src={img3} text="Phones are destroying our productivty" label="Productivity" path="/blog"/>
-                        <CardItem src={img4} text="Boosting your mood with music, ultimately increasing productivity" label="Mood" path="/blog"/>
-                        <CardItem src={img8} text="Bitcoin, Bitclout, and other Cryptos are taking over" label="Crypto" path="/blog"/>
-                    </ul>
-                </div>
-            </div>
+  const [postData, setPostData] = useState(null);
+  let postData_chunkOne = null;
+  let postData_chunkTwo = null;
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "post"]{
+                title,
+                slug,
+                mainImage{
+                    asset->{
+                        _id,
+                        url     
+                    },
+                    alt
+                },
+                excerpt,
+                "name": author->name,
+                publishedAt,
+                categories,
+            }`
+      )
+      .then((data) => {
+        setPostData(data);
+      })
+      .catch(console.error);
+  }, []);
+
+  if (postData) {
+    console.log({ postData });
+    postData_chunkOne = postData.slice(0, 2);
+    postData_chunkTwo = postData.slice(2, 5);
+
+    console.log({ postData_chunkOne, postData_chunkTwo });
+  }
+
+  return (
+    <div className="cards">
+      <h1>Check Out These Featured Articles</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            {postData_chunkOne &&
+              postData_chunkOne.map((post, index) => (
+                <CardItem
+                  src={post.mainImage.asset.url}
+                  text="This is the main two articles placeholder content."
+                  label="Technology"
+                  path={"/blog/" + post.slug.current}
+                  key={post.slug.current}
+                />
+              ))}
+          </ul>
+          <ul className="cards__items">
+            {postData_chunkTwo &&
+              postData_chunkTwo.map((post, index) => (
+                <CardItem
+                  src={post.mainImage.asset.url}
+                  text="This is the sub articles placeholder content."
+                  label="Technology"
+                  path={"/blog/" + post.slug.current}
+                  key={post.slug.current}
+                />
+              ))}
+          </ul>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Cards
+export default Cards;
