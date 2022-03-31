9bd06cafd32c2019e3d59030dcb9f1c8d01eeb0f
DavidReider <freakdj1997@yahoo.com>
2022-03-31 19:16:44 -0400

    Update Homepage Link

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,20 +1,33 @@
-import React from 'react';
-import '../App.css';
-import { Button } from './Button';
-import './HeroSection.css';
+import React from "react";
+import "../App.css";
+import { Button } from "./Button";
+import "./HeroSection.css";
 
 function HeroSection() {
-    return (
-        <div className='hero-container'>
-            <video src="./videos/video-3.mp4" autoPlay loop muted></video>
-            <h1>CODEBUG</h1>
-            <p>What are you waiting for?</p>
-            <div className="hero-btns">
-                <Button className="btns" buttonStyle='btn--outline' buttonSize='btn--large'>Begin Reading</Button>
-                <Button className="btns" buttonStyle='btn--primary' buttonSize='btn--large'>GET STARTED</Button>
-            </div>
-        </div>
-    )
+  return (
+    <div className="hero-container">
+      <video src="./videos/video-3.mp4" autoPlay loop muted></video>
+      <h1>CODEBUG</h1>
+      <p>What are you waiting for?</p>
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+          tolink="blog"
+        >
+          Begin Reading
+        </Button>
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+        >
+          GET STARTED
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default HeroSection;
